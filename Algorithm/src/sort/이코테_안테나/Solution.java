fddbc2d8cb5cede2be01e9401ef3a15da0828b74
jungbin97 <ljb5054@gmail.com>
2023-04-15 02:40:59 +0900

    [이코테] 안테나 (구현, java) 시간 복잡도 개선

--- Algorithm/src/sort/이코테_안테나/Solution.java
+++ Algorithm/src/sort/이코테_안테나/Solution.java
@@ -17,42 +17,64 @@ public class Solution {
          *  4. 각 값중 최소 거리를 가지는 집 위치 반환
          *  시간복잡도 : O(NlogN) + O(N^2) => O(N^2)
          **********************************************************************/
+//        public static void main(String[] args) throws IOException{
+//                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//                // 집 개수 입력 받기
+//                int N = Integer.parseInt(br.readLine());
+//
+//                // 집위치 배열
+//                HashSet<Integer> arr = new HashSet<>();
+//                // 입력 받기
+//                StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//                for(int i = 0; i<N; i++){
+//                        arr.add(Integer.valueOf(st.nextToken()));
+//                }
+//
+//                // 정렬(HashSet(순서를 보장하지 않으므로) => List로 변환)
+//                ArrayList<Integer> arr1 = new ArrayList<>(arr);
+//                arr1.sort(Comparator.naturalOrder());   // 오름 차순 정렬
+//
+//
+//                int result = 100001;
+//                int antannaLocation = 0;
+//                for(Integer i : arr1){
+//                        int distanceSum = 0;
+//                        int a = i;    // 안테나 설치 위치
+//                        for (Integer j : arr1){
+//                                int b = j;    // 비교할 위치
+//                                distanceSum += Math.abs(b-a); // 거리의 총합
+//                        }
+//
+//                        if(result > distanceSum){        // 작으면 현재 안테 설치위치 저장
+//                                result = distanceSum;
+//                                antannaLocation = a;
+//                        }
+//                }
+//                System.out.println(antannaLocation);
+//        }
+        /**********************************************************************
+         * 가장 중간위치에 있는 집이 모든 거리 총합에 대해 최소값이므로
+         * 가장 중간위치의 집 위치를 출력한다.
+         * 시간 복잡도 : O(NlogN)
+         **********************************************************************/
         public static void main(String[] args) throws IOException{
                 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-                // 집 개수 입력 받기
+                // 집 개수 입력
                 int N = Integer.parseInt(br.readLine());
 
-                // 집위치 배열
-                HashSet<Integer> arr = new HashSet<>();
-                // 입력 받기
+                ArrayList<Integer> arrayList = new ArrayList<>();
                 StringTokenizer st = new StringTokenizer(br.readLine());
-
                 for(int i = 0; i<N; i++){
-                        arr.add(Integer.valueOf(st.nextToken()));
+                        arrayList.add(Integer.parseInt(st.nextToken()));
                 }
 
-                // 정렬(HashSet(순서를 보장하지 않으므로) => List로 변환)
-                ArrayList<Integer> arr1 = new ArrayList<>(arr);
-                arr1.sort(Comparator.naturalOrder());   // 오름 차순 정렬
+                // 정렬 (오름차순)
+                Collections.sort(arrayList);
 
-
-                int result = 100001;
-                int antannaLocation = 0;
-                for(Integer i : arr1){
-                        int distanceSum = 0;
-                        int a = i;    // 안테나 설치 위치
-                        for (Integer j : arr1){
-                                int b = j;    // 비교할 위치
-                                distanceSum += Math.abs(b-a); // 거리의 총합
-                        }
-
-                        if(result > distanceSum){        // 작으면 현재 안테 설치위치 저장
-                                result = distanceSum;
-                                antannaLocation = a;
-                        }
-
-                }
-                System.out.println(antannaLocation);
+                // 중간값 출력
+                System.out.println(arrayList.get((N-1)/2));
         }
 }
