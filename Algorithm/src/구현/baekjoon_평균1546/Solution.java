00bfc3b1fb4d7ba69aeb9853892540705059e1b0
jungbin97 <ljb5054@gmail.com>
2023-07-03 10:07:40 +0900

    [baekjoon-1546]평균 (구현, java)

---
+++ Algorithm/src/구현/baekjoon_평균1546/Solution.java
@@ -0,0 +1,30 @@
+package 구현.baekjoon_평균1546;
+
+import java.util.Scanner;
+
+public class Solution {
+        /**********************************************************************
+         *
+         *
+         **********************************************************************/
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int N = sc.nextInt();
+            int A[] = new int[N];
+
+            for (int i = 0; i<N; i++){
+                A[i] = sc.nextInt();
+            }
+
+            int sum = 0;
+            int max = 0;
+            // 최댓값 찾기
+            for(int i = 0; i<N; i++){
+                if (A[i] > max){
+                    max = A[i];
+                }
+                sum += A[i];
+            }
+            System.out.println(sum*100.0/max/N);
+        }
+}
