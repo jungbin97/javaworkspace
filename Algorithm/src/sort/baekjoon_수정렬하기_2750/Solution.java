d20762a6084d63b7c6886e94e619627a96f0bd48
jungbin97 <ljb5054@gmail.com>
2023-07-06 18:46:03 +0900

    [baekjoon-2750]수 정렬하기 (정렬, java)

---
+++ Algorithm/src/sort/baekjoon_수정렬하기_2750/Solution.java
@@ -0,0 +1,37 @@
+package sort.baekjoon_수정렬하기_2750;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+        /**********************************************************************
+         *
+         *
+         **********************************************************************/
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            int N = Integer.parseInt(br.readLine());
+            int[] arr = new int[N];
+
+            for (int i = 0; i<N; i++){
+                arr[i] = Integer.parseInt(br.readLine());
+            }
+
+            // 버블 정렬
+            for(int i=0; i<N; i++){
+                for(int j = 0; j<N-i-1; j++){
+                    if (arr[j] > arr[j+1]){
+                        int temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+                }
+            }
+
+            for(int i =0; i<N; i++){
+                System.out.println(arr[i]);
+            }
+        }
+}
