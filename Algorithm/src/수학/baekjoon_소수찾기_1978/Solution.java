308bfc35ec2091d79b2e46cc7358d95137154c9f
ego2 <ljb5054@gmail.com>
2023-08-09 21:11:45 +0900

    [baekjoon_1978] 소수찾기(수학, java)

---
+++ Algorithm/src/수학/baekjoon_소수찾기_1978/Solution.java
@@ -0,0 +1,43 @@
+package 수학.baekjoon_소수찾기_1978;
+
+import java.util.Scanner;
+
+public class Solution {
+        /**********************************************************************
+         *
+         *
+         **********************************************************************/
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int cnt = 0;
+
+            int[] arr = new int[n];
+
+            for (int i =0; i<n; i++){
+                int tmp = sc.nextInt();
+                arr[i] = tmp;
+            }
+
+            for (int i = 0; i<n; i++){
+                // 1제외 하기
+                if(arr[i]==0 || arr[i]==1){
+                    continue;
+                }
+
+                if (prime(arr[i])){
+                    cnt += 1;
+                }
+            }
+            System.out.println(cnt);
+        }
+
+    private static boolean prime(int x) {
+            for(int i = 2; i < Math.ceil(Math.sqrt(x)+1); i++){
+                if (x%i==0){
+                    return false;
+                }
+            }
+            return true;
+    }
+}
