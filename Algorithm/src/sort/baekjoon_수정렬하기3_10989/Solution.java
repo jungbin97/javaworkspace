02e2d783e421ac699c650fb4e3530f5d9346dbc3
ego2 <ljb5054@gmail.com>
2023-08-10 20:24:56 +0900

    [baekjoon_10989] 수 정렬하기3(정렬, java) - 시간 초과

---
+++ Algorithm/src/sort/baekjoon_수정렬하기3_10989/Solution.java
@@ -0,0 +1,31 @@
+package sort.baekjoon_수정렬하기3_10989;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+        /**********************************************************************
+         *
+         *
+         **********************************************************************/
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[10001];
+
+            for (int i=0; i<n;i++){
+                 int num = Integer.parseInt(br.readLine());
+                 arr[num] += 1;
+            }
+
+            for (int i = 0; i<10001; i++){
+                if (arr[i]!= 0){
+                    for (int j = 0; j<arr[i]; j++){
+                        System.out.println(i);
+                    }
+                }
+            }
+        }
+}
